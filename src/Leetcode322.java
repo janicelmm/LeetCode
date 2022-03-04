3897d3959eedaf2a07410d8982c290e11fa17863
janicelmm <janicelmm@gmail.com>
2022-03-03 20:52:08 -0500

    Updates to leetcode322
    Updates to solution LC322

--- src/Leetcode322.java
+++ src/Leetcode322.java
@@ -1,5 +1,10 @@
-public class Leetcode322 {
+/*
+ * Use DP to find the min coins needed to from amount $0 to $amount. 
+ * Need to loop through the coins array, and keep track of the min number of coins needed. 
+ * Trick is to set the max value to be (amount + 1), as it's impossible to get greater than this value. 
+ */
 
+public class Leetcode322 {
     public static int coinChange(int[] coins, int amount) {
     	
     	if(amount == 0)
@@ -38,9 +43,37 @@ public class Leetcode322 {
     	
     }
     
+    public static int coinChange2(int[] coins, int amount) {
+               
+        if (amount == 0)
+        {
+            return 0;
+        }
+        
+        int[] track = new int[amount + 1];     
+        
+        for (int j = 1; j < track.length; j++)
+        {   
+            track[j] = amount + 1;
+            
+            for (int c : coins)
+            {
+                if (j >= c)
+                {
+                   track[j] = Math.min(1 + track[j - c], track[j]);       
+                }
+            }
+        }
+        
+        if (track[amount] < amount + 1)
+        {
+            return track[amount];
+        }
+        
+        return -1;
+    }
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(coinChange(new int[] {2}, 3));
+		System.out.println(coinChange2(new int[] {186,419,83,408}, 6249));
 	}
-
 }
