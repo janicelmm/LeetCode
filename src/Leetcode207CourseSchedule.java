e27584f73741a9c8eec58036ca2b2ec46d96883a
janicelmm <janicelmm@gmail.com>
2022-03-08 22:42:04 -0500

    Update to LC207
    Update to LC207

--- src/Leetcode207CourseSchedule.java
+++ src/Leetcode207CourseSchedule.java
@@ -5,65 +5,121 @@
  * Loop through the array again, and check if all courses required do not have preReq anymore.
  */
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Leetcode207CourseSchedule {
-	public static boolean canFinish(int numCourses, int[][] prerequisites) {
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] track = new int[numCourses];
-        
-        for (int[] i : prerequisites)
+
+        for (int[] i : prerequisites) 
         {
             track[i[0]]++;
         }
-        
+
         Queue<Integer> noPre = new LinkedList<>();
-        
-        for (int j = 0; j < track.length; j++)
+
+        for (int j = 0; j < track.length; j++) 
         {
-            if (track[j] == 0)
+            if (track[j] == 0) 
             {
                 noPre.offer(j);
             }
         }
-        
-        while (!noPre.isEmpty())
+
+        while (!noPre.isEmpty()) 
         {
             int current = noPre.poll();
-            
-            for (int[] k : prerequisites)
+
+            for (int[] k : prerequisites) 
             {
-                if (track[k[0]] == 0)
+                if (track[k[0]] == 0) 
                 {
                     continue;
                 }
-                    
-                if (k[1] == current)
+
+                if (k[1] == current) 
                 {
                     track[k[0]]--;
                 }
-                
-                if (track[k[0]] == 0)
+
+                if (track[k[0]] == 0) 
                 {
                     noPre.offer(k[0]);
                 }
             }
         }
-        
-        for(int a = 0; a < track.length; a++)
+
+        for (int a = 0; a < track.length; a++) 
         {
-            if (track[a] != 0)
+            if (track[a] != 0) 
             {
                 return false;
             }
         }
-        
+
         return true;
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(canFinish(2, new int[][] {{1,0}}));
-	}
 
+    public static boolean canFinish2(int numCourses, int[][] prerequisites) {
+        Queue<int[]> pre = new LinkedList<>();
+        int[] courses = new int[numCourses];
+        HashSet<Integer> noPre = new HashSet<>();
+
+        for (int[] i : prerequisites) 
+        {
+            courses[i[0]]++;
+        }
+
+        for (int j = 0; j < courses.length; j++) 
+        {
+            if (courses[j] == 0) 
+            {
+                noPre.add(j);
+            }
+        }
+
+        for (int[] k : prerequisites) 
+        {
+            if (noPre.contains(k[1])) 
+            {
+                pre.offer(k);
+            }
+        }
+
+        while (!pre.isEmpty()) 
+        {
+            int[] temp = pre.poll();
+            int canTake = temp[0];
+            courses[canTake]--;
+            if (courses[canTake] == 0) 
+            {
+                noPre.add(canTake);
+
+                for (int[] k : prerequisites) 
+                {
+                    if (k[1] == canTake) 
+                    {
+                        pre.offer(k);
+                    }
+                }
+            }
+        }
+
+        for (int course : courses) 
+        {
+            if (course != 0) 
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canFinish(2, new int[][] {{1, 0}}));
+    }
 }
