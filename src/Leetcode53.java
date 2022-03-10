d8726a8381bb5bec7d8a9a5aece97fd9b29d3ad5
janicelmm <janicelmm@gmail.com>
2022-03-09 23:11:05 -0500

    Update LC53
    Update LC53

--- src/Leetcode53.java
+++ src/Leetcode53.java
@@ -3,30 +3,26 @@
  */
 
 public class Leetcode53 {
-	public static int maxSubArray(int[] nums) {
-        
-		int sum = nums[0];
-		int max = nums[0];
-		
-		for (int i = 1; i < nums.length; i++)
-		{
-			sum = Math.max(sum + nums[i], nums[i]);
-			max = Math.max(max, sum);
-		}
-		
-		return max;
-		
-	}
-	
-	public static void main(String[] args) {
-	    System.out.println(maxSubArray(new int[] {5,4,-1,7,8}));
-	
-    	//-2,1,-3,4,-1,2,1,-5,4
-    	//-1, 0, -2
-    	//-2,-1
-    	//-1,-2,-3,0
-    	//-1,1,2,1
-    	//-2,-3,-1
-	
-	}
+    public static int maxSubArray(int[] nums) {
+        int sum = nums[0];
+        int max = nums[0];
+    
+        for (int i = 1; i < nums.length; i++) 
+        {
+            sum = Math.max(sum + nums[i], nums[i]);
+            max = Math.max(max, sum);
+        }
+    
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[] {5,4,-1,7,8}));
+        // -2,1,-3,4,-1,2,1,-5,4
+        // -1, 0, -2
+        // -2,-1
+        // -1,-2,-3,0
+        // -1,1,2,1
+        // -2,-3,-1
+    }
 }
