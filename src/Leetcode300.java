9d78b0a2edb47cc4dd477c471f2abb736715e6e5
janicelmm <janicelmm@gmail.com>
2022-03-09 23:27:19 -0500

    Update to LC300
    Update to LC300

--- src/Leetcode300.java
+++ src/Leetcode300.java
@@ -1,34 +1,32 @@
 
 public class Leetcode300 {
-	public static int lengthOfLIS(int[] nums) {
-	     
+    public static int lengthOfLIS(int[] nums) {
+
         int max = 1;
         int[] count = new int[nums.length];
         count[0] = 1;
-        
-        for (int i = 1; i < nums.length; i++)
+
+        for (int i = 1; i < nums.length; i++) 
         {
-            for (int j = i-1; j >=0; j--)
+            for (int j = i - 1; j >= 0; j--) 
             {
-                if (nums[i] > nums[j])
+                if (nums[i] > nums[j]) 
                 {
                     count[i] = Math.max(count[j] + 1, count[i]);
-                }
-                else
+                } 
+                else 
                 {
                     count[i] = Math.max(count[i], 1);
                 }
-                
-                max = Math.max(count[i], max);
-            }                
-        }
-        
-        return max;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(lengthOfLIS(new int[] {10,9,2,5,3,7,101,18}));
-	}
 
+                max = Math.max(count[i], max);
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLIS(new int[] { 10, 9, 2, 5, 3, 7, 101, 18 }));
+    }
 }
