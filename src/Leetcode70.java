ceef3856fc492822f4c2dc663b24246792330009
janicelmm <janicelmm@gmail.com>
2022-03-09 23:21:47 -0500

    Update to LC70
    Update to LC70

--- src/Leetcode70.java
+++ src/Leetcode70.java
@@ -1,41 +1,43 @@
 
 public class Leetcode70 {
 
-    public static int climbStairs(int n) {
-        if(n <= 2)
+    public static int climbStairs(int n) 
+    {
+        if (n <= 2) 
         {
             return n;
-        }
-        else
+        } 
+        else 
         {
-            return climbStairs(n-1) + climbStairs(n-2);
+            return climbStairs(n - 1) + climbStairs(n - 2);
         }
     }
-    
-    public int climbStairs2(int n) {
-        if(n <= 2)
+
+    public int climbStairs2(int n) 
+    {
+        if (n <= 2) 
         {
             return n;
         }
-        
-        int[] count = new int[n+1];
-        
-        for (int i = 0; i < count.length; i++)
+
+        int[] count = new int[n + 1];
+
+        for (int i = 0; i < count.length; i++) 
         {
-            if (i <= 2)
+            if (i <= 2) 
             {
                 count[i] = i;
-            }
-            else
+            } 
+            else 
             {
-                count[i] = count[i-1] + count[i-2];
+                count[i] = count[i - 1] + count[i - 2];
             }
         }
-        
+
         return count[n];
     }
-    
-	public static void main(String[] args) {
-		System.out.println(climbStairs(10));
-	}
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(10));
+    }
 }
