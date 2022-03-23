67ef2102b163acf361b62cb2b40315a44977db42
janicelmm <janicelmm@gmail.com>
2022-03-22 22:56:34 -0400

    Updates to LC79
    Updates to LC79

--- src/Leetcode79WordSearch.java
+++ src/Leetcode79WordSearch.java
@@ -1,7 +1,10 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /* Strategy: To use a helper function to check the adj character once the first character is found.
  * While statement is not necessarily needed for a recursive method. If one inner loop returns true, and the outer loop returns false, the true is lost. 
  * By checking index == word.length() - 1, I am making sure index will not go out of bound already.
- * Too keep track of boolean answer, we can use "||" i.e ans = ans || boolean function, so it will always track the true. 
+ * To keep track of boolean answer, we can use "||" i.e ans = ans || boolean function, so it will always track the true. 
  * To add the out of bound check that applies to all i+1, i-1, j+1, j-1, use another helper function.
  */
 
@@ -62,9 +65,73 @@ public class Leetcode79WordSearch {
     	
     }
     
+    public static boolean exist2(char[][] board, String word) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean ans = false;
+        int index = 0;
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[0].length; j++)
+            {
+                if (board[i][j] == word.charAt(0))
+                {
+                    q.offer(new int[]{i, j});
+                }
+            }
+        }
+        
+        while (!q.isEmpty())
+        {
+            int[] temp = q.poll();
+            ans = explore(temp[0], temp[1], word, board, index, visited);
+            
+            if (ans == true)
+            {
+                return ans;
+            }
+        }
+        
+        return ans;
+    }
+    
+    public static boolean explore(int i, int j, String word, char[][] board, int index, boolean[][] visited)
+    {
+        int[] dir = {0, -1, 0, 1, 0};
+        
+        if (board[i][j] != word.charAt(index))
+        {
+            return false;
+        }
+        
+        if (index == word.length() - 1)
+        {
+            return true;
+        }
+        
+        boolean found = false;
+        visited[i][j] = true;
+        
+        if (i >= 0 && j >= 0 && i < board.length && j < board[0].length)
+        {
+            for (int k = 0; k < dir.length - 1; k++)
+            {
+                int x = i + dir[k];
+                int y = j + dir[k+1];
+                
+                if (x >= 0 && y >= 0 && x < board.length && y < board[0].length && visited[x][y] == false)
+                {
+                    found = explore(x, y, word, board, index + 1, visited) || found;                      
+                }
+            }
+        }
+        
+        visited[i][j] = false;
+        return found;
+    }
+    
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(exist(new char[][] {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}}, "ABCCED"));
 	}
-
 }
